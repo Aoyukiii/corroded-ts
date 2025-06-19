cc94b036e096115135fd3821097ae4a448ad991e
0yuki <147019313+Aoyukiii@users.noreply.github.com>
2025-06-19 23:22:28 +0800

    fix: fix type inferencing bug

--- src/lib/promise.ts
+++ src/lib/promise.ts
@@ -2,11 +2,11 @@ import { ResultAsync } from './result'
 
 declare global {
   interface Promise<T> {
-    toResult(): ResultAsync<T>
+    toResult<E>(): ResultAsync<T, E>
   }
 }
 
-Promise.prototype.toResult = function <T>(): ResultAsync<T> {
+Promise.prototype.toResult = function <T, E>(): ResultAsync<T, E> {
   return ResultAsync.from(this)
 }
 
